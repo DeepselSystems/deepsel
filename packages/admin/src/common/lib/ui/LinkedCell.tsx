235c36b7b52f1d30cfbdc268b96d5b2a89413480
pak <anhkhuong975@gmail.com>
2026-08-17 17:29:08 +0700

    Harden Role module

--- packages/admin/src/common/lib/ui/LinkedCell.tsx
+++ packages/admin/src/common/lib/ui/LinkedCell.tsx
@@ -1,5 +1,4 @@
 import { Link, type LinkProps } from 'react-router-dom';
-import clsx from 'clsx';
 
 interface LinkedCellProps extends Omit<LinkProps, 'to'> {
   /** DataGrid row params (used to infer `to` if not provided) */
@@ -12,7 +11,7 @@ interface LinkedCellProps extends Omit<LinkProps, 'to'> {
  */
 export const LinkedCell = ({ params, className, to, children, ...props }: LinkedCellProps) => {
   return (
-    <Link className={clsx(className)} to={to ?? params?.row.id.toString() ?? ''} {...props}>
+    <Link className={className} to={to ?? params?.row.id.toString() ?? ''} {...props}>
       {children}
     </Link>
   );
